18842c832ce9271ae4d42df9a7f4c7db58c1584c
pc559 <clarkeph@tcd.ie>
2021-07-07 11:46:20 +0100

    Reduced to work done

--- save_wc.sh
+++ save_wc.sh
@@ -6,4 +6,8 @@ echo "$page_count"
 relative_wc=$(bc <<< "scale=2; ${word_count}/60000.")
 echo "$relative_wc"
 echo "$(date),$word_count,$page_count" >> word_counts.csv
+git add -u
+read -p "Commit description: " desc
+git commit -m "$desc"
+git push
 python3 wc_plotter.py
