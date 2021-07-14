50a625ba61cdfe526f93147b5a5d3133c2be4bb5
pc559 <clarkeph@tcd.ie>
2021-07-14 11:41:27 +0100

    test credentials

--- save_wc.sh
+++ save_wc.sh
@@ -1,3 +1,4 @@
+read -p "Commit description: " desc
 make
 word_count=$(ps2ascii thesis.pdf | wc -w)
 page_count=$(pdftk thesis.pdf dump_data | grep NumberOfPages | cut -d' ' -f 2)
@@ -8,7 +9,6 @@ relative_wc=$(bc <<< "scale=2; ${word_count}/60000.")
 echo "$relative_wc"
 echo "$(date),$word_count,$page_count" >> word_counts.csv
 git add -u
-read -p "Commit description: " desc
 git commit -m "$desc"
 git push
 python3 wc_plotter.py
