b66167c6a29c443f86b3d1a924dc5019d243b247
pc559 <clarkeph@tcd.ie>
2021-07-14 11:44:52 +0100

    Added desc to word_counts

--- save_wc.sh
+++ save_wc.sh
@@ -3,7 +3,7 @@ make
 word_count=$(ps2ascii thesis.pdf | wc -w)
 page_count=$(pdftk thesis.pdf dump_data | grep NumberOfPages | cut -d' ' -f 2)
 relative_wc=$(bc <<< "scale=2; ${word_count}/60000.")
-echo "$(date),$word_count,$page_count" >> word_counts.csv
+echo "$(date),$word_count,$page_count,$desc" >> word_counts.csv
 git add -u
 git commit -m "$desc"
 git push
