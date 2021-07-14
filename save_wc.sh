1cba4724205a75c5d5f173dd995b1c3bcb32e139
pc559 <clarkeph@tcd.ie>
2021-07-14 11:43:31 +0100

    misc

--- save_wc.sh
+++ save_wc.sh
@@ -2,13 +2,13 @@ read -p "Commit description: " desc
 make
 word_count=$(ps2ascii thesis.pdf | wc -w)
 page_count=$(pdftk thesis.pdf dump_data | grep NumberOfPages | cut -d' ' -f 2)
-echo "$(date)"
-echo "$word_count"
-echo "$page_count"
 relative_wc=$(bc <<< "scale=2; ${word_count}/60000.")
-echo "$relative_wc"
 echo "$(date),$word_count,$page_count" >> word_counts.csv
 git add -u
 git commit -m "$desc"
 git push
+echo "$(date)"
+echo "$word_count"
+echo "$page_count"
+echo "$relative_wc"
 python3 wc_plotter.py
