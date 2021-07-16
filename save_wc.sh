f962c89ab4c666e4e187857c178b47458ca172db
pc559 <clarkeph@tcd.ie>
2021-07-16 13:51:10 +0100

    misc

--- save_wc.sh
+++ save_wc.sh
@@ -4,6 +4,7 @@ make
 word_count=$(ps2ascii thesis.pdf | wc -w)
 page_count=$(pdftk thesis.pdf dump_data | grep NumberOfPages | cut -d' ' -f 2)
 relative_wc=$(bc <<< "scale=2; ${word_count}/60000.")
+next_percent=$(bc <<< "${relative_wc}*60000+600")
 echo "$(date),$word_count,$page_count,$desc" >> word_counts.csv
 git add -u
 git commit -m "$desc"
@@ -12,4 +13,5 @@ echo "$(date)"
 echo "$word_count"
 echo "$page_count"
 echo "$relative_wc"
+echo "$(bc <<< ${next_percent}-${word_count})"
 python3 wc_plotter.py
