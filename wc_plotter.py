0f48dad52ac6db14f267b2c992c7101ecb37f4c7
pc559 <clarkeph@tcd.ie>
2021-07-19 11:53:19 +0100

    Added extrapolation of word count

--- wc_plotter.py
+++ wc_plotter.py
@@ -1,10 +1,22 @@
 import pandas as pd
+from pandas.tseries.offsets import BDay
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 import numpy as np
+import sys
 fname = "word_counts.csv"
 data = pd.read_csv(fname)
 data['Date'] = pd.to_datetime(data['Date'])
+
+timescale = BDay(2)#pd.Timedelta(5, 'days')
+last_week = data['WordCount'][data['Date'] > data['Date'].iloc[-1]-timescale]
+delta_y = data['WordCount'][data['Date'] == data['Date'].iloc[-1]]-last_week.iloc[0]
+delta_x = timescale
+words_per_day = delta_y/delta_x.n#.days
+words_left = 60000-data['WordCount'][data['Date'] == data['Date'].iloc[-1]]
+days_left = words_left/words_per_day
+print('Weeks left:', days_left.values[0]/5)
+
 #print(data.head())
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
@@ -17,7 +29,7 @@ ax2.plot(data['Date'], data['PageCount'], 'rx-', label='Page count')
 ax2.set_ylabel('Page count')
 ax2.yaxis.tick_right()
 ax2.yaxis.set_label_position("right")
-date_form = DateFormatter("%d-%m")
+date_form = DateFormatter("%m-%d %Hh")
 ax1.xaxis.set_major_formatter(date_form)
 ax2.xaxis.set_major_formatter(date_form)
 ax1.set_ylim(0, 60000)
