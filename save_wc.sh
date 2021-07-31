ea6b1e9557c63501befc448ba5c78e8ff2981824
pc559 <clarkeph@tcd.ie>
2021-07-31 18:27:38 +0100

    Fiona wholesomeness

--- save_wc.sh
+++ save_wc.sh
@@ -15,4 +15,6 @@ echo "$page_count"
 echo "$relative_wc"
 echo "Only $(bc <<< ${next_percent}-${word_count}) words until the next percent."
 python3 wc_plotter.py
+echo ""
+echo "IMPT FIONA MSG"
 python3 read_fionas_wholesomeness.py test.txt
