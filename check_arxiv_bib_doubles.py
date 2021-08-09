766e9f9f2cf01bb13d75d86fad959dd9d9d02fd0
pc559 <clarkeph@tcd.ie>
2021-08-09 12:06:37 +0100

    misc

--- check_arxiv_bib_doubles.py
+++ check_arxiv_bib_doubles.py
@@ -3,12 +3,14 @@ fname = 'thesis.bib'
 with open(fname) as bib:
     raw_lines = bib.readlines()
     lines = [line for line in raw_lines if ('eprint' in line and not 'eprinttype' in line)]
-    print(lines[:5])
+    for i, line in enumerate(lines[:5]):
+        print(i, line.strip())
     print('Number of eprints:', len(lines))
     lines = ["".join(filter(str.isdigit, line)) for line in lines]
-    print(lines[:5])
+    for i, line in enumerate(lines[:5]):
+        print(i, line)
     eprints = set(lines)
     print('Number of unique eprints:', len(eprints))
     author_lines = [line for line in raw_lines if 'author' in line]
-    print(len(author_lines))
+    print('Number of author lines  :', len(author_lines))
 
