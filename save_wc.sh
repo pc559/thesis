21cabde6a10b39b2559d99ff5eb1a918d8ad0c04
pc559 <clarkeph@tcd.ie>
2021-08-16 13:26:43 +0100

    misc

--- save_wc.sh
+++ save_wc.sh
@@ -3,8 +3,8 @@ read -p "Commit description: " desc
 make
 word_count=$(ps2ascii thesis.pdf | wc -w)
 page_count=$(pdftk thesis.pdf dump_data | grep NumberOfPages | cut -d' ' -f 2)
-relative_wc=$(bc <<< "scale=2; ${word_count}/60000.")
-next_percent=$(bc <<< "${relative_wc}*60000+600")
+relative_wc=$(bc <<< "scale=2; ${word_count}/50000.")
+next_percent=$(bc <<< "${relative_wc}*50000+500")
 echo "$(date),$word_count,$page_count,$desc" >> word_counts.csv
 git add -u
 git commit -m "$desc"
