b0172df50d08cf4f143c6d6216623f28968a3bb6
pc559 <clarkeph@tcd.ie>
2021-11-08 14:22:23 +0000

    corrections in red

--- save_wc.sh
+++ save_wc.sh
@@ -14,7 +14,7 @@ echo "$word_count"
 echo "$page_count"
 echo "$relative_wc"
 echo "Only $(bc <<< ${next_percent}-${word_count}) words until the next percent."
-python3 wc_plotter.py
+#python3 wc_plotter.py
 echo ""
 echo "IMPT FIONA MSG"
 python3 read_fionas_wholesomeness.py PHIL.txt
